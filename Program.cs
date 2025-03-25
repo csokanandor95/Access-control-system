7e441d0476ca0e83dd9bd4de84f699e1f4c82d72
csokanandor95 <csokanandor77@gmail.com>
2025-03-25 23:09:35 +0100

    Created Tevekenyseg class, constructor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    class Tevekenyseg //1 sorban 1 tevékenység van, ezt jelképezi az osztály
+    {
+        public string Azon { get; set; } //azonosító
+        public DateTime Ido { get; set; } //időpont
+        public int Kod { get; set; } //kód
+        public Tevekenyseg(string azon, DateTime ido, int kod) //konstruktor
+        {
+            Azon = azon;
+            Ido = ido;
+            Kod = kod;
+        }
+    }
 
+    static void ElsoFeladat()
+    {
+        using (StreamReader sr = new StreamReader("bedat.txt"))
+        {
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split(" ");
+                
+            }
+        }
+    static void Main()
+    {
+
+    }
+
+}
 
