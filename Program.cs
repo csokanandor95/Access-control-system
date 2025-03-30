f576691712b0a69ee43d0aba764cda925b424330
csokanandor95 <csokanandor77@gmail.com>
2025-03-30 22:11:44 +0200

    HarmadikFeladat done

--- Program.cs
+++ Program.cs
@@ -34,14 +34,33 @@
     }
     static void MasodikFeladat()
     {
-        Console.WriteLine("2. feladat:");
+        Console.WriteLine("2. feladat:"); // $ string interpoláció: idézőjelek közötti szövegben bele lehet ágyazni változókat vagy kifejezéseket anélkül, hogy külön kellene őket összefűzni
         Console.WriteLine($"2. feladat: Az elso tanulo {tevekenysegek[0].Ido.Hour:02}:{tevekenysegek[0].Ido.Minute:02} -kor lepett be a fokapun. "); //2 számjegyű formátumban kell megadni az órát és a percet
         Console.WriteLine($"2. feladat: Az utolso tanulo {tevekenysegek[tevekenysegek.Count -1].Ido.Hour:02}:{tevekenysegek[tevekenysegek.Count - 1].Ido.Minute:02} -kor lepett be a fokapun. "); //.count-al az utolsó elem indexe megadható max algoritmus helyett
     }
 
-    static void Main() //program belépési pontja, itt fogjuk meghívni a metódusokat
+    static void HarmadikFeladat()
+    {
+        Console.WriteLine("3. feladat:");
+        using (StreamWriter sw = new StreamWriter("kesok.txt")) //fájl írása a későkről
+        {
+            for (int i = 0; i < tevekenysegek.Count; i++) //végigmegyünk a tevékenységeken
+            {
+                DateTime kezdido = new(1, 1, 1, 7, 50, 0); //kezdőidő 7:50
+                DateTime vegido = new(1, 1, 1, 8, 15, 0); //végeidő 8:15
+                if (tevekenysegek[i].Kod == 1 && tevekenysegek[i].Ido > kezdido && tevekenysegek[i].Ido <= vegido) //ha a kód 1, azaz belépés történt, és az idő a megadott intervallumban van = késés történt
+                {
+                    sw.WriteLine($"{tevekenysegek[i].Ido.Hour:02}:{tevekenysegek[i].Ido.Minute:02} {tevekenysegek[i].Azon}"); //kiírjuk a késők azonosítóját
+                }
+
+            }
+        }
+    }
+
+            static void Main() //program belépési pontja, itt fogjuk meghívni a metódusokat
     {
         MasodikFeladat();
+        HarmadikFeladat();
     }
 }
 
